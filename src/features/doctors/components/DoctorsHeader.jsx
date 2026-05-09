9d1b96c2d20356595570d901152f2ebdfb90f111
MariamMohammed0011 <maream.mohamad.0938856942@gmail.com>
2026-05-09 06:19:45 -0700

    our trait

--- src/features/doctors/components/DoctorsHeader.jsx
+++ src/features/doctors/components/DoctorsHeader.jsx
@@ -3,22 +3,23 @@ import { Search } from 'lucide-react';
 
 const DoctorsHeader = ({ searchQuery, onSearchChange }) => {
   return (
-    <div className="flex justify-between w-[60%]  items-center mb-8 gap-6" dir="rtl">
+    <div className="flex justify-between w-[70%] absolute top-1 right-0   items-center mb-8 gap-6" dir="rtl">
       {/* عنوان الصفحة */}
-      <div className="bg-white px-12 py-3.5 rounded-2xl font-black text-text-main text-lg border border-gray-100 shadow-sm">
-        الاطباء
+      <div className="px-12 py-2">
+        <h1 className="text-3xl md:text-4xl font-extrabold text-main tracking-tight">
+          الأطباء
+        </h1>  
       </div>
-
       {/* حقل البحث */}
-      <div className="relative flex-grow max-w-xl">
+      <div className="relative flex-grow max-w-xl ">
         <input 
           type="text" 
           placeholder="بحث.." 
           value={searchQuery}
           onChange={(e) => onSearchChange(e.target.value)}
-          className="w-full bg-white border border-gray-100 py-3.5 pr-12 pl-6 rounded-2xl shadow-sm focus:outline-none focus:ring-2 focus:ring-primary/10 text-text-main font-medium placeholder-gray-400"
+          className="w-full bg-white border border-gray-100 py-2 pr-3 pl-6 rounded-[1rem] shadow-sm focus:outline-none focus:ring-2 focus:ring-primary/10 text-text-main font-medium placeholder-gray-400"
         />
-        <Search className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400" size={20} />
+        <Search className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400" size={20} />
       </div>
     </div>
   );
